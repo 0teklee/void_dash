e3892a9cd80a7c792811cb51c6858f952513d168
tedlee <ted.lee@bclabs.co.kr>
2023-07-21 02:30:46 +0900

    wip: removed unused var

--- app/api/list/route.ts
+++ app/api/list/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/prisma/prismaClient";
 
-export async function GET(req: Request) {
+export async function GET() {
   try {
     const posts = await prisma.post.findMany({
       take: 20,
