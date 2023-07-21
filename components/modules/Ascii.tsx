7c2d39faf69eb76595c6bea692a090c1b1f82cb9
tedlee <ted.lee@bclabs.co.kr>
2023-07-22 04:55:14 +0900

    style: fixed style ascii component

--- components/modules/Ascii.tsx
+++ components/modules/Ascii.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Ascii = ({ text, color }: { text: string; color?: string }) => {
   return (
     <div
-      className={`relative w-fit max-w-2xl h-[400px] m-2 ${
+      className={`relative w-fit max-w-2xl h-full m-2 ${
         color || `text-primary`
-      } text-[5px] font-light leading-none`}
+      } text-[5px] sm:text-[6px] font-light leading-none`}
     >
       <pre>
         <code>{text}</code>
