96e6ebda73aa5fbc45571be1f0e7355c0595c2d4
tedlee <ted.lee@bclabs.co.kr>
2023-07-21 17:35:43 +0900

    fix: prisma client fixed

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,13 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma = new PrismaClient();
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
-
-if (process.env.NODE_ENV === "development") globalForPrisma.prisma = prisma;
-
-export default globalForPrisma.prisma;
+export default prisma;
