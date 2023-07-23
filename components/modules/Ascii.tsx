d1fbed5d8e12b526d844f578d1d91d7f807227cc
tedlee <ted.lee@bclabs.co.kr>
2023-07-23 17:29:22 +0900

    style: ascii styling class applied

--- components/modules/Ascii.tsx
+++ components/modules/Ascii.tsx
@@ -5,7 +5,7 @@ const Ascii = ({ text, color }: { text: string; color?: string }) => {
     <div
       className={`relative mt-12 w-screen sm:w-fit h-full m-2 ${
         color || `text-primary`
-      }  text-[1px] md:text-md font-light leading-none`}
+      }  text-ascii font-light leading-none`}
     >
       <pre>
         <code>{text}</code>
