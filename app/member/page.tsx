e41b714434fe4e72cfad9d3d10b6017bb83661d5
tedlee <ted.lee@bclabs.co.kr>
2023-07-23 22:10:20 +0900

    fix: add header on member page

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -4,6 +4,7 @@ import { signIn, useSession } from "next-auth/react";
 import { motion, useAnimationControls } from "framer-motion";
 import { ouroboro } from "@/libs/client/ascii";
 import { terms } from "@/libs/client/values";
+import Header from "@/components/modules/Header";
 
 const Page = () => {
   const [direction, setDirection] = useState(1);
@@ -36,68 +37,71 @@ const Page = () => {
   };
 
   return (
-    <div
-      className={`flex flex-col gap-3 justify-center items-center my-20 text-primary w-full h-full`}
-    >
-      <h1 className={`text-6xl font-semibold line-through`}>Members</h1>
-      <div className={`text-10xs md:text-member-md`}>
-        <motion.div animate={controls} className={`leading-none`}>
-          <pre>
-            <code>{ouroboro}</code>
-          </pre>
-        </motion.div>
-      </div>
-      {!sessionData && (
-        <>
-          <div className={`my-5`}>
-            <button
-              disabled={!agree}
-              className={`p-2 mb-5 text-red-500 bg-black border border-primary rounded disabled:opacity-30 disabled:text-primary`}
-              onClick={async () => {
-                await login();
-              }}
-            >
-              Signup with Google
-            </button>
-          </div>
-          <div className={`flex items-center gap-3`}>
-            <p
-              className={`text-sm`}
-            >{`I agree with the terms and conditions`}</p>
-            <input
-              type={`checkbox`}
-              className={`bg-black focus:ring-primary`}
-              onChange={(e) => {
-                setAgree(e.target.checked);
-              }}
-            />
-          </div>
-          <div>
-            <div className={`flex items-center gap-3 text-primary`}>
-              <p className={`text-sm`}>Terms and conditions</p>
+    <>
+      <Header />
+      <div
+        className={`flex flex-col gap-3 justify-center items-center my-20 text-primary w-full h-full`}
+      >
+        <h1 className={`text-6xl font-semibold line-through`}>Members</h1>
+        <div className={`text-10xs md:text-member-md`}>
+          <motion.div animate={controls} className={`leading-none`}>
+            <pre>
+              <code>{ouroboro}</code>
+            </pre>
+          </motion.div>
+        </div>
+        {!sessionData && (
+          <>
+            <div className={`my-5`}>
               <button
-                onClick={() => {
-                  setIsToggle(!isToggle);
+                disabled={!agree}
+                className={`p-2 mb-5 text-red-500 bg-black border border-primary rounded disabled:opacity-30 disabled:text-primary`}
+                onClick={async () => {
+                  await login();
                 }}
               >
-                {isToggle ? "△" : "⛛"}
+                Signup with Google
               </button>
             </div>
-          </div>
-          {isToggle && (
-            <div className={`max-w-[400px] text-center text-xs`}>
-              <p className={`mb-2`}>{terms.en}</p>
-              <p>{terms.kr}</p>
+            <div className={`flex items-center gap-3`}>
+              <p
+                className={`text-sm`}
+              >{`I agree with the terms and conditions`}</p>
+              <input
+                type={`checkbox`}
+                className={`bg-black focus:ring-primary`}
+                onChange={(e) => {
+                  setAgree(e.target.checked);
+                }}
+              />
             </div>
-          )}
-        </>
-      )}
-      {!!sessionData && (
-        <>
-          <p className={`text-2xl font-semibold`}>you are in</p>
-        </>
-      )}
-    </div>
+            <div>
+              <div className={`flex items-center gap-3 text-primary`}>
+                <p className={`text-sm`}>Terms and conditions</p>
+                <button
+                  onClick={() => {
+                    setIsToggle(!isToggle);
+                  }}
+                >
+                  {isToggle ? "△" : "⛛"}
+                </button>
+              </div>
+            </div>
+            {isToggle && (
+              <div className={`max-w-[400px] text-center text-xs`}>
+                <p className={`mb-2`}>{terms.en}</p>
+                <p>{terms.kr}</p>
+              </div>
+            )}
+          </>
+        )}
+        {!!sessionData && (
+          <>
+            <p className={`text-2xl font-semibold`}>you are in</p>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
