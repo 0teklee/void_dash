f57d161b89e5c0610e1f0bf746eb9b339baa86b7
tedlee <ted.lee@bclabs.co.kr>
2023-08-01 23:35:50 +0900

    fix: session api fixed

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -4,9 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
-
   return NextResponse.json({
     authenticated: !!session,
-    session,
   });
 }
