00740378aa8311d8071e3c554a09a43afcb3ddca
tedlee <ted.lee@bclabs.co.kr>
2023-08-01 23:36:13 +0900

    fix: api fetching fixed

--- app/void/[id]/page.tsx
+++ app/void/[id]/page.tsx
@@ -3,8 +3,13 @@ import Header from "@/components/modules/Header";
 import DetailTemplate from "@/components/templates/DetailTemplate";
 import { getAllPostId } from "@/libs/server/listApi";
 import { notFound } from "next/navigation";
-import { getContentImg } from "@/utils/formatter";
-import { getPostData } from "@/libs/server/detailApi";
+import { getContentImg, htmlReplace } from "@/utils/formatter";
+import Head from "next/head";
+
+const getDetailPost = async (id: string) => {
+  const data = await fetch(`${process.env.NEXT_APP_URL}/api/post/${id}`);
+  return await data.json();
+};
 
 const Page = async (props: {
   params: {
@@ -16,12 +21,46 @@ const Page = async (props: {
   }
 
   const postId = props.params.id as string;
-  const data = await getPostData(Number(postId));
+  const data = await getDetailPost(postId);
 
   return (
     <>
-      <Header />
-      <main>{<DetailTemplate data={data} searchParams={postId} />}</main>
+      <Head>
+        <meta></meta>
+        <title>{data?.post?.title || `VOID_${props.params.id}`}</title>
+        <meta
+          property="og:title"
+          content={data?.post?.title || `VOID_${props.params.id}`}
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:description"
+          content={`${htmlReplace(data?.post?.content)?.slice(0, 150) || ""}`}
+        />
+        <meta
+          property="og:image"
+          content={getContentImg(data?.post?.content) || `/horror.jpg`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:url"
+          content={`${process.env.NEXT_APP_URL}/void/${props.params.id}`}
+        />
+        <meta
+          name="twitter:image"
+          content={getContentImg(data?.post?.content) || `/horror.jpg`}
+        />
+        <meta name="twitter:image:alt" content={`void`} />
+        <meta
+          name="twitter:title"
+          content={data?.post?.title || `VOID_${props.params.id}`}
+        />
+        <meta name="twitter:creator" content="_void" />
+      </Head>
+      <body>
+        <Header />
+        <main>{<DetailTemplate data={data} searchParams={postId} />}</main>
+      </body>
     </>
   );
 };
@@ -38,7 +77,7 @@ export const generateStaticParams = async () => {
 };
 
 export async function generateMetadata(props: { params: { id: string } }) {
-  const data = await getPostData(Number(props?.params?.id));
+  const data = await getDetailPost(props?.params?.id);
   if (!data) {
     return notFound();
   }
